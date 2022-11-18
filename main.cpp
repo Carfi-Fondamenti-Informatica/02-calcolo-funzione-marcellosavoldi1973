b71340b8afdad3d34e243a39f4c0d4122fd844d4
marcellosavoldi1973 <118396911+marcellosavoldi1973@users.noreply.github.com>
2022-11-18 11:31:15 +0100

    Update main.cpp
    aggiunto codice per risoluzione esercizio

--- main.cpp
+++ main.cpp
@@ -1,6 +1,47 @@
+#include <cstdlib>
 #include <iostream>
-using namespace std;
 
-int main() {
-   return 0;
+//Marcello Savoldi 57130621
+
+int main(int argc, char* argv[])
+{
+    double a = 0.0;
+    double b = 0.0;
+    double x = 0.0;
+    double y = 0.0;
+
+// used DOUBLE instead FLOAT to increase the resolution
+
+    std::cout <<"exercise by Marcello Savoldi 57130621\n";
+    std::cout << "insert a = ";
+    std::cin >> a;
+    std::cout << "insert b = ";
+    std::cin >> b;
+    std::cout << "insert x = ";
+    std::cin >> x;
+    std::cout << "insert y = ";
+    std::cin >> y;
+
+
+    double z = 0.0;
+
+
+    if (x < 0.0 && y > 0.0)
+    {
+        z = a * x - b * y;
+    }
+    else if (x >= 0.0 && y <= 0.0)
+    {
+        z = a * x * x - b * y;
+    }
+    else
+    {
+        z = a * x - b * y * y;
+    }
+
+
+    std::cout << std::endl << "result z = " << z << std::endl;
+
+
+    return 0;
 }
